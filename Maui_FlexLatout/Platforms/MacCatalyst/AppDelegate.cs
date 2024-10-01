a970212359bbe1a875613f3948d1d7a195128d23
Donghwi_Park <gptnc5100@gmail.com>
2024-10-01 16:54:14 +0200

    Add project files.

---
+++ Maui_FlexLatout/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Maui_FlexLatout
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
